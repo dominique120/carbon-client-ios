5d0ab10b3b3c3e480c55569eb37252ce80c40cf7
SueOcho <64862065+SueOcho@users.noreply.github.com>
2020-10-13 19:48:25 -0700

    Borders

---
+++ InputConConstraints/TFUIView.swift
@@ -0,0 +1,55 @@
+//
+//  TFUIView.swift
+//  InputConConstraints
+//
+//  Created by user179030 on 10/13/20.
+//  Copyright © 2020 Kenyi Rodriguez. All rights reserve
+
+import UIKit
+
+@IBDesignable class TFUIView: UITextField {
+    
+    @IBInspectable var conerRadius: CGFloat = 0 {
+        didSet {
+            self.layer.cornerRadius = self.conerRadius
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor = .black {
+        didSet {
+            self.layer.borderColor = self.borderColor.cgColor
+        }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat = 0 {
+        didSet {
+            self.layer.borderWidth = self.borderWidth
+        }
+    }
+    
+    @IBInspectable var shadowColor: UIColor = .black {
+        didSet {
+            self.layer.shadowColor = self.shadowColor.cgColor
+        }
+    }
+    
+    @IBInspectable var shadowOffset: CGSize = .zero {
+        didSet {
+            self.layer.shadowOffset = self.shadowOffset
+        }
+    }
+    
+    @IBInspectable var shadowRadius: CGFloat = 0 {
+        didSet {
+            self.layer.shadowRadius = self.shadowRadius
+        }
+    }
+    
+    @IBInspectable var shadowOpacity: Float = 0 {
+        didSet {
+            self.layer.shadowOpacity = self.shadowOpacity
+        }
+    }
+    
+}
+
---
+++ InputConConstraints/VUIView.swift
@@ -0,0 +1,54 @@
+//
+//  VUIView.swift
+//  InputConConstraints
+//
+//  Created by user179030 on 10/13/20.
+//  Copyright © 2020 Kenyi Rodriguez. All rights reserved.
+//
+import UIKit
+
+@IBDesignable class VUIView: UIView {
+    
+    @IBInspectable var conerRadius: CGFloat = 0 {
+        didSet {
+            self.layer.cornerRadius = self.conerRadius
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor = .black {
+        didSet {
+            self.layer.borderColor = self.borderColor.cgColor
+        }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat = 0 {
+        didSet {
+            self.layer.borderWidth = self.borderWidth
+        }
+    }
+    
+    @IBInspectable var shadowColor: UIColor = .black {
+        didSet {
+            self.layer.shadowColor = self.shadowColor.cgColor
+        }
+    }
+    
+    @IBInspectable var shadowOffset: CGSize = .zero {
+        didSet {
+            self.layer.shadowOffset = self.shadowOffset
+        }
+    }
+    
+    @IBInspectable var shadowRadius: CGFloat = 0 {
+        didSet {
+            self.layer.shadowRadius = self.shadowRadius
+        }
+    }
+    
+    @IBInspectable var shadowOpacity: Float = 0 {
+        didSet {
+            self.layer.shadowOpacity = self.shadowOpacity
+        }
+    }
+    
+}
