e7af58a71a394de29f99907bfd81d24c71c1d533
SueOcho <64862065+SueOcho@users.noreply.github.com>
2020-12-06 20:57:46 -0800

    Update Creat acount,create my pos

---
+++ InputConConstraints/MyPost.swift
@@ -0,0 +1,16 @@
+//
+//  MyPost.swift
+//  InputConConstraints
+//
+//  Created by user178961 on 12/6/20.
+//  Copyright © 2020 Kenyi Rodriguez. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MyPost : UIViewController{
+    override var preferredStatusBarStyle: UIStatusBarStyle{
+        return.lightContent
+    }
+}
