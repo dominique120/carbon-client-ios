38c8c4e7a8fcca3d56acaaaa59124852735ae501
SueOcho <64862065+SueOcho@users.noreply.github.com>
2020-12-07 20:08:49 -0800

    create profileFollow view

---
+++ InputConConstraints/ProfileFollow.swift
@@ -0,0 +1,14 @@
+//
+//  ProfileFollow.swift
+//  InputConConstraints
+//
+//  Created by user178961 on 12/7/20.
+//  Copyright © 2020 Kenyi Rodriguez. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ProfileFollow:UIViewController{
+    
+}
